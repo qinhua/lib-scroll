12bff119dc508d0447e62232cbef95954b64c634
zhuxun.jb <zhuxun.jb@taobao.com>
2014-01-14 19:05:15 +0800

    更新手势库

--- build/combo.debug.js
+++ build/combo.debug.js
@@ -1,2 +1,2 @@
-document.write('<scr' + 'ipt id="combo_url_1389583299896" type="text/javascript" src="http://g.assets.daily.taobao.net/mtb/??lib-motion/1.0.0/motion.debug.js,lib-gesture/1.0.0/gesture.debug.js"></scr' + 'ipt>')
+document.write('<scr' + 'ipt id="combo_url_1389697435703" type="text/javascript" src="http://g.assets.daily.taobao.net/mtb/??lib-motion/1.0.0/motion.debug.js,lib-gesture/1.1.0/gesture.debug.js"></scr' + 'ipt>')
 document.write('<scr' + 'ipt id="combo_url_main" type="text/javascript" src="build/scroll.debug.js"></scr' + 'ipt>')
