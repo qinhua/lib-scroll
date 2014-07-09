b13068fb79c84bd3e8daec073b2af48d1e73a85f
zhuxun.jb <zhuxun.jb@taobao.com>
2014-07-09 22:35:10 +0800

    修改获取元素高宽的方式

--- build/combo.debug.js
+++ build/combo.debug.js
@@ -1,1 +1,1 @@
-document.write('<scr' + 'ipt id="combo_url_1402454047155" type="text/javascript" src="http://g.assets.daily.taobao.net/mtb/??lib-motion/1.0.1/motion.debug.js,lib-gesture/1.1.4/gesture.debug.js"></scr' + 'ipt>')
+document.write('<scr' + 'ipt id="combo_url_1404916493606" type="text/javascript" src="http://g.assets.daily.taobao.net/mtb/??lib-motion/1.0.1/motion.debug.js,lib-gesture/1.1.4/gesture.debug.js"></scr' + 'ipt>')
