e5acda07cfb7adc3a54e4882db42783baa1fbc2c
zhuxun.jb <zhuxun.jb@taobao.com>
2014-08-23 19:55:02 +0800

    cmdwrap

--- Gruntfile.js
+++ Gruntfile.js
@@ -78,43 +78,29 @@ module.exports = function(grunt) {
                 },
                 dest: '<%= distPath%>/combo.js'
             }
+        },
+
+        cmdwrap: {
+            js: {
+                files: [{
+                    expand: true,
+                    cwd: '<%= distPath %>',
+                    src: ['scroll.js', 'plugin.js'],
+                    dest: '<%= distPath %>',
+                    ext: '.cmd.js'
+                }]
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-depconcat');
     grunt.loadNpmTasks('grunt-depcombo');
+    grunt.loadNpmTasks('grunt-cmdwrap');
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-watch');
-    
-    grunt.registerTask('cmdwrap', 'wrap for CMD', function() {
-        var path = require('path');
-        var done = this.async();
-        var type = 'lib';
-        var name = 'scroll';
-        var files = ['scroll', 'plugin'];
-        var distPath = 'build';
-        var pkgJson = grunt.file.readJSON('package.json');
-        var version = pkgJson.version;
-
-        for (var i = 0; i < files.length; i++) {
-            var file = files[i];
-            var jsFile = grunt.file.read(path.join(distPath, file + '.js'));
-            jsFile += ('\nif (window.KISSY) {' +
-                            'KISSY.add(\'mtb/{{type}}-{{name}}/{{version}}/{{file}}.cmd\', window.{{type}}.{{name}});' +
-                        '} else if (\'undefined\' !== typeof define) {' +
-                            'define(\'mtb/{{type}}-{{name}}/{{version}}/{{file}}.cmd\', [], window.{{type}}.{{name}});' +
-                        '}').replace(/\{\{type\}\}/g, type)
-                        .replace(/\{\{name\}\}/g, name)
-                        .replace(/\{\{file\}\}/g, file)
-                        .replace(/\{\{version\}\}/g, version);
-            grunt.file.write(path.join(distPath, file + '.cmd.js'), jsFile);
-        }
-
-        done();
-    });
-
+ 
     grunt.registerTask('dist', ['copy', 'depconcat', 'uglify', 'depcombo', 'cmdwrap']);
     grunt.registerTask('dev', ['watch']);
     
--- build/combo.debug.js
+++ build/combo.debug.js
@@ -1,1 +1,2 @@
-document.write('<scr' + 'ipt id="combo_url_1408543004678" type="text/javascript" src="http://g.assets.daily.taobao.net/mtb/??lib-motion/1.0.1/motion.debug.js,lib-gesture/1.1.7/gesture.debug.js"></scr' + 'ipt>')
+document.write('<scr' + 'ipt id="combo_url_1408794886579" type="text/javascript" src="http://g.assets.daily.taobao.net/mtb/??lib-gesture/1.1.7/gesture.debug.js,lib-motion/1.0.1/motion.debug.js"></scr' + 'ipt>')
+document.write('<scr' + 'ipt id="combo_url_main" type="text/javascript" src="build/scroll.debug.js"></scr' + 'ipt>')
